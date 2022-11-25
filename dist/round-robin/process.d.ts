e4fcc4dfff34e2149cfe157a6608d08d001a3097
Javohir <javohirabdusattorov2005@gmail.com>
2022-11-25 16:40:49 +0500

    [-] Removed ignored files and folders

--- dist/round-robin/process.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Process {
-    private name;
-    private state;
-    private execution;
-    quantum: number;
-    constructor(name: string);
-    start(): void;
-    stop(): void;
-}
--- dist/scheduling/algorithms/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from "./round-robin";
-export * from "./priority";
